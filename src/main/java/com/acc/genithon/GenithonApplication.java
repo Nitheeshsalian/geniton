7178ccde2db2fab4ff15c171daf0ba6a8fb11b3f
Prabu Amaresan <prabu.amaresan@accenture.com>
2024-01-30 11:41:31 +0530

    Initial commit

---
+++ src/main/java/com/acc/genithon/GenithonApplication.java
@@ -0,0 +1,13 @@
+package com.acc.genithon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenithonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenithonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acc/genithon/GenithonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.genithon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenithonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
